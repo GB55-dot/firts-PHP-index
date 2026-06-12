91a2ca02562e2171659294ca67fff213bb41ed20
GB55 <bazillion.gudima222@gmail.com>
2026-06-13 00:18:23 +0300

    First

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+</head>
+<body>
+<?php
+    echo "<b>Hello</b> World!";
+    echo "<br>Something else";
+
+    echo "\"";
+
+    // This is a comment
+    # This is also a comment
+    /*
+        This is a multi-line comment
+        It can span multiple lines
+    */
+?>
+</body>
+</html>
