de46b14c794552fe5ab6c067845b7cc547982780
GB55 <bazillion.gudima222@gmail.com>
2026-06-15 13:36:06 +0300

    Sixth lesson

---
+++ Switch-case.php
@@ -0,0 +1,17 @@
+<?php
+  $x = 42;
+  switch($x) {
+    case 5:
+        echo 'Num is 5';
+        break;
+    case 10:
+        echo 'Num is 10';
+        break;
+    case 50:
+        echo 'Num is 50';
+        break;
+    default:
+        echo 'Num is not 5, 10 or 50';
+        break;
+  }
+?>
