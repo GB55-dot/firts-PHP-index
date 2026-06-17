f5a33c52e67c2153eba49d805b4609b6737a29cb
GB55 <bazillion.gudima222@gmail.com>
2026-06-17 21:42:04 +0300

    Thirteenth lesson

--- block/header.php
+++ block/header.php
@@ -7,6 +7,13 @@
             content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $title; ?></title>
+    <style>
+        body {
+            background: #333;
+            color: #fff;
+            margin: 20px;
+        }
+    </style>
 </head>
 <body>
 <header>
--- index.php
+++ index.php
@@ -4,5 +4,12 @@ require "block/header.php";
 ?>
 <h1>Home Page</h1>
 <?php
-  require "block/footer.php";
+  date_default_timezone_set('Europe/Kiev');
+  // echo date('t F');
+  echo date('m-l Час: H:i:s', time() + 1000).'<br>';
+  echo time(). '<br>';
+  echo date('m-l Час: H:i:s', strtotime('+1 hours - 2 days')).'<br>';
+  echo date('m-l Час: H:i:s', strtotime('next Month')).'<br>';
+
+  require_once "block/footer.php";
 ?>
